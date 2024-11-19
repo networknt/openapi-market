baf0c7faf7b43dab916583644b48e00c5856f6ef
Steve Hu <stevehu@gmail.com>
2024-11-19 14:40:58 -0500

    update from the light-example-4j

--- src/main/java/com/networknt/market/handler/StoreProductsGetHandler.java
+++ src/main/java/com/networknt/market/handler/StoreProductsGetHandler.java
@@ -1,33 +1,69 @@
 package com.networknt.market.handler;
 
-import com.networknt.body.BodyHandler;
+import com.networknt.client.http.Http2ServiceRequest;
 import com.networknt.config.Config;
 
+import com.networknt.config.JsonMapper;
 import com.networknt.handler.LightHttpHandler;
-import com.networknt.http.HttpMethod;
-import com.networknt.http.HttpStatus;
-import com.networknt.http.MediaType;
+import com.networknt.http.*;
+import com.networknt.market.MarketConfig;
+import com.networknt.market.ServiceRef;
+import com.networknt.status.Status;
+import com.networknt.utility.Constants;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 import io.undertow.util.HeaderMap;
+import io.undertow.util.HttpString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.net.URI;
 import java.util.Deque;
+import java.util.List;
 import java.util.Map;
 
-/**
-For more information on how to write business handlers, please check the link below.
-https://doc.networknt.com/development/business-handler/rest/
-*/
 public class StoreProductsGetHandler implements LightHttpHandler {
 
-    
+    private static final Logger logger = LoggerFactory.getLogger(StoreProductsGetHandler.class);
+    public static final String referenceAPI = "referenceAPI";
+    public static final String GENERIC_EXCEPTION = "ERR10014";
+
+    MarketConfig config = (MarketConfig) Config.getInstance().getJsonObjectConfig(MarketConfig.CONFIG_NAME, MarketConfig.class);
+
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        // HeaderMap requestHeaders = exchange.getRequestHeaders();
-        // Map<String, Deque<String>> queryParameters = exchange.getQueryParameters();
-        String responseBody = "[{\"id\":1,\"name\":\"catten\",\"tag\":\"cat\"},{\"id\":2,\"name\":\"doggy\",\"tag\":\"dog\"}]";
-        exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        exchange.setStatusCode(HttpStatus.OK.value());
-        exchange.getResponseSender().send(responseBody);
+        HeaderMap responseHeaders = new HeaderMap();
+        responseHeaders.add(Headers.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+        try {
+            Http2ServiceRequest getStoreData = getHttp2ServiceRequest(referenceAPI);
+            List list = getStoreData.callForTypedObject(List.class).get();
+            exchange.setStatusCode(HttpStatus.OK.value());
+            exchange.getResponseSender().send(JsonMapper.toJson(list));
+        } catch (Exception e) {
+            logger.error("API call error: ", e);
+            Status status = new Status(GENERIC_EXCEPTION, e.getMessage());
+            setExchangeStatus(exchange, status);
+        }
+    }
+
+    private ServiceRef getService(String name) {
+        return config.getApiServiceRef().get(name);
+    }
+
+    private Http2ServiceRequest getHttp2ServiceRequest(String serviceName) throws Exception {
+        ServiceRef serviceRef = getService(serviceName);
+        if(serviceRef == null) throw new Exception("Missing service config in market.yml");
+        String proxyUrl = config.getProxyUrl();
+        if(logger.isDebugEnabled()) logger.debug("proxyUrl = " + proxyUrl);
+        Http2ServiceRequest http2ServiceRequest = new Http2ServiceRequest(new URI(proxyUrl), serviceRef.getPath(), new HttpString(serviceRef.getMethod()));
+        http2ServiceRequest.addRequestHeader("Host", "localhost");
+        http2ServiceRequest.addRequestHeader(Constants.TRACEABILITY_ID_STRING, "traceabilityId");
+        if(serviceRef.getServiceUrl() != null) {
+            http2ServiceRequest.addRequestHeader(Constants.SERVICE_URL_STRING, serviceRef.getServiceUrl());
+        } else {
+            http2ServiceRequest.addRequestHeader(Constants.SERVICE_ID_STRING, serviceRef.getServiceId());
+        }
+        return http2ServiceRequest;
     }
 }
--- src/main/java/com/networknt/market/handler/StoreProductsPostHandler.java
+++ src/main/java/com/networknt/market/handler/StoreProductsPostHandler.java
@@ -11,22 +11,22 @@ import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 import io.undertow.util.HeaderMap;
 import com.networknt.market.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Deque;
 import java.util.Map;
 
-/**
-For more information on how to write business handlers, please check the link below.
-https://doc.networknt.com/development/business-handler/rest/
-*/
 public class StoreProductsPostHandler implements LightHttpHandler {
+    private static final Logger logger = LoggerFactory.getLogger(StoreProductsPostHandler.class);
 
-    
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
         // HeaderMap requestHeaders = exchange.getRequestHeaders();
         // Map<String, Deque<String>> queryParameters = exchange.getQueryParameters();
         Map<String, Object> bodyMap = (Map<String, Object>)exchange.getAttachment(BodyHandler.REQUEST_BODY);
         Product requestBody = Config.getInstance().getMapper().convertValue(bodyMap, Product.class);
+        if(logger.isDebugEnabled()) logger.debug(requestBody.toString());
         String responseBody = "{}";
         exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         exchange.setStatusCode(HttpStatus.OK.value());
